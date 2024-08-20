a884301da4918e5592edfc02cd7566c610dccb7a
Tiago França <t.iago_196@hotmail.com>
2024-08-20 00:18:00 -0300

    Add project files.

---
+++ Inner_Factory/Point.cs
@@ -0,0 +1,28 @@
+﻿namespace Inner_Factory
+{
+    public class Point
+    {
+
+        public double x;
+        public double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+    }
+}
---
+++ Inner_Factory/Program.cs
@@ -0,0 +1,11 @@
+﻿using Inner_Factory;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var cartesianPoint = Point.Factory.NewCartesianPoint(3, 4);
+
+        var polarPoint = Point.Factory.NewPolarPoint(5, Math.PI / 4);
+    }
+}
