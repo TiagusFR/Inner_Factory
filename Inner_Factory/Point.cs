e18be014a900c8407e30e8a83b9a733431043614
Tiago França <t.iago_196@hotmail.com>
2024-08-20 01:19:03 -0300

    Correcting_Point_Class_Visibility

--- Inner_Factory/Point.cs
+++ Inner_Factory/Point.cs
@@ -3,10 +3,10 @@
     public class Point
     {
 
-        public double x;
-        public double y;
+        private double x;
+        private double y;
 
-        public Point(double x, double y)
+        private Point(double x, double y)
         {
             this.x = x;
             this.y = y;
